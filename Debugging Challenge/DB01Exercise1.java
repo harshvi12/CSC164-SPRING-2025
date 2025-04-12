6a5269ea10af332576db3d31cae176f7ccb6dce5
harshvi <harshvi1210@gmail.com>
2025-04-11 20:48:58 -0400

    Update DB01Exercise1.java

--- Debugging Challenge/DB01Exercise1.java
+++ Debugging Challenge/DB01Exercise1.java
@@ -1,6 +1,5 @@
 /*
-Group Members: Harshvi
-
+Name: Harshvi
 Write a Java program to get a number from the user and print whether it is positive or negative.
  */
 
@@ -10,25 +9,25 @@ public class DB01Exercise1 {
 
     public static void main(String[] args)
     {
-        // Bug : 'System' used instead of 'System.in'
-        Scanner in = new Scanner(System);
+        // Fixed: Changed 'System' to 'System.in'
+        Scanner in = new Scanner(System.in);
 
         System.out.print("Input number: ");
 
-        // Bug : 'in.next()' returns a String, but assigned to an int variable
-        int input = in.next();
+        // Fixed: Changed 'in.next()' to 'in.nextInt()' to capture integer input
+        int input = in.nextInt();
 
         if (input > 0)
         {
             System.out.println("Number is positive");
         }
-        // Bug : This condition includes zero, so the next condition is unreachable
-        else if (input <= 0)
+        // Fixed: Changed condition to check for negative numbers
+        else if (input < 0)
         {
             System.out.println("Number is negative");
         }
-        // Bug : 'else if' has no condition, causing a syntax error
-        else if
+        // Fixed: Used 'else' instead of 'else if' for zero
+        else
         {
             System.out.println("Number is zero");
         }
