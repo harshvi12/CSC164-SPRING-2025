20d0597b97ccc8b81d1cb638548a398767f3344e
harshvi <harshvi1210@gmail.com>
2025-04-25 12:01:37 -0400

    Create Product.java

---
+++ Module 10LabListView/Product.java
@@ -0,0 +1,32 @@
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // toString
+    public String toString() {
+        return "Product: " + name + " | Price: $" + price;
+    }
+}
