fecdfdaee8dca5abbfd1733f8ec6c7908d0fadf5
harshvi <harshvi1210@gmail.com>
2025-04-29 17:54:12 -0400

    Create Product.java

---
+++ Module 6-7 Assignment: Classes and Objects II /Product.java
@@ -0,0 +1,33 @@
+public class Product {
+    private String name;
+    private double price;
+
+    // Constructor
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Price: $" + price;
+    }
+}
